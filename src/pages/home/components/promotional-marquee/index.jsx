c889c0ae9c8a61500b0483a039507701a0ec9182
salf1-sabit <salfisabit@gmail.com>
2024-08-07 02:17:55 +0600

    bugfix replace star icon with centered-dot

--- src/pages/home/components/promotional-marquee/index.jsx
+++ src/pages/home/components/promotional-marquee/index.jsx
@@ -6,12 +6,12 @@ const PromotionalMarquee = () => {
     <Wrapper className="promotional-marquee mt-[8vw] text-nowrap font-medium uppercase sm:mt-[5vw] md:mt-0">
       <div className="marquee">
         <div className="marquee-group pl-[1em]">
-          ✨ Perfect Designs - Where Uniforms Meet Perfection! ✨ Experience the
-          Best in Quality and Service.
+          &bull; Perfect Designs - Where Uniforms Meet Perfection! &bull;
+          Experience the Best in Quality and Service
         </div>
         <div className="marquee-group pl-[1em]">
-          ✨ Perfect Designs - Where Uniforms Meet Perfection! ✨ Experience the
-          Best in Quality and Service.
+          &bull; Perfect Designs - Where Uniforms Meet Perfection! &bull;
+          Experience the Best in Quality and Service
         </div>
       </div>
     </Wrapper>
